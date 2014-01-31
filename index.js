290496d858f522a11df54fb662ad73d3c64ee164
Payam Abedi <payam@virtru.com>
2014-01-31 17:28:00 -0500

    added bootstrap

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 // Require all of the plugins
-require('jquery-trigger-native');
-require('jquery-first-child');
-require('jquery-waiter');
-require('simplemodal');
-require('jquery-ellipsis');
-require('bootstrap');
-
-
-var jQuery = require('jquery');
-
+var jQuery = require('bootstrap');
 //Export jquery
 module.exports = jQuery;

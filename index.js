d050e78653bd2883194263fb5cb14ff7f61f41c4
Payam Abedi <payam@virtru.com>
2014-04-07 13:09:24 -0400

    updated jquery-pack-fmw to be a proper component, fixed jquery deps

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// Require all of the plugins
-var jQuery = require('bootstrap');
-
+require('bootstrap');
 require('jquery-ellipsis');
+var jQuery = require('jquery');
 
 //Export jquery
 module.exports = jQuery;
